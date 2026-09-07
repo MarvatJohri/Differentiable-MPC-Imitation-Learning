Fixed controller normalizing issue

imitation_dagger.py
@@ -83,7 +83,7 @@ from functools import partial
 
 
 # Experiment params
-RL_EXPERIMENT_NAME = "spacecraft_ppo_v1_torque_only"
+RL_EXPERIMENT_NAME = "spacecraft_ppo_omega_hard_limit_test"
 EXPERIMENT_NAME = "spacecraft_ppo_imitation_dagger_v1_torque_only_experiment1"
 EXPERIMENT_NOTES = "Initial imitation learning on Earth orbit"
 
@@ -408,6 +408,9 @@ def loss_fn(controller: DiffMPCController,
                                                  data_batch["nominal_cntrl"])
 
     predicted_actions = predicted_actions / MAX_TORQUE  # Scale predicted actions to [-1, 1] range for loss computation
+
+
+
 
     loss = jnp.mean((predicted_actions - data_batch["expert_actions"]) ** 2)      
                           
@@ -1086,8 +1089,8 @@ def test_jax_env():
 if __name__ == "__main__":
 
 
-    # main()
+    main()
 
     # test_jax_env()
 
-    dry_test()
+    # dry_test()

simulation_env_jax.py
@@ -373,7 +373,7 @@ def generate_trajectory(env: SpacecraftEnvJax,
     # Generate initial nominal trajectories
     key, subkey = jax.random.split(key)
     nominal_traj = jnp.tile(init_env_state.state, (horizon + 1, 1))
-    nominal_cntrl = 1e-3 * jax.random.normal(subkey, shape=(horizon, nu), dtype=jnp.float64)
+    nominal_cntrl = 1e-8 * jax.random.normal(subkey, shape=(horizon, nu), dtype=jnp.float64)
 
     init_carry = (init_env_state, init_obs, 0, nominal_traj, nominal_cntrl, key)
 